bcfd950b0110f2c7db2b46bf90ac60af80a054dd
bbreton09 <bbreton@gmail.com>
2017-04-04 10:18:17 +0200

    Update httprequest.js
    Ajout de la fonction Json pour parcourir les scénarios avec le filtre par groupe (Jeeble)

--- js/httprequest.js
+++ js/httprequest.js
@@ -1,8 +1,9 @@
 // Load Box value on start
 
 function initJeedomValue() {
-localStorage.setItem("IP", "192.168.1.x");
-localStorage.setItem("KEY", "2ycN5TGJasfakjghPaK8");
+localStorage.setItem("IP", "https://jeedom");
+localStorage.setItem("KEY", "macléAPI");
+requestscenes("Jeeble");
 }
 
 // Basic HTTP request to launch command
@@ -31,3 +32,42 @@ client.onreadystatechange = function() {
 client.send();
 };
 
+function requestscenes(jeedomScenes){
+	var myKeyValue, myIPValue;
+	
+	myKeyValue =  localStorage.getItem("KEY");
+	myIPValue =  localStorage.getItem("IP");
+	console.log("KEY Value: " + myKeyValue);
+	console.log("IP Value: " + myIPValue);
+	var element = document.getElementById('scenes');
+	 
+	var getJSON = function(url) {
+		return new Promise(function(resolve, reject) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('get', url, true);
+			xhr.responseType = 'json';
+			xhr.onload = function() {
+				var status = xhr.status;
+				if (status == 200) {
+					resolve(xhr.response);
+				} else {
+					reject(status);
+				}
+			};
+			xhr.send();
+		});
+	};
+
+	getJSON(myIPValue +'/core/api/jeeApi.php?request={"method":"scenario::all","params":{"apikey":"'+ myKeyValue +'"},"jsonrpc":"2.0"}').then(function(data) {   
+		//console.log(data.result);
+		var trHTML = '';
+		for (var i in data.result) {
+			console.log(data.result[i])
+			if (data.result[i].group == jeedomScenes) {
+				element.innerHTML = trHTML += '<li><a href="'+myIPValue +'/core/api/jeeApi.php?apikey='+ myKeyValue +'&type=scenario&id='+ data.result[i].id +'&action=start">'+ data.result[i].name +'</a></li>';
+			}
+		}
+	}, function(status) {
+		console.log('il y a une erreur');
+	});
+};
